1663fffb9827167293c9f0cecd2c959748f37d4e
gjeanmart <gregoire.jeanmart@consensys.net>
2018-09-06 11:48:32 +0100

    Configure a `LegacyCookieProcessor` to accept cookie domain such as `.domain.com`

---
+++ server/src/main/java/net/consensys/web3auth/configuration/WebConfiguration.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package net.consensys.web3auth.configuration;
+
+import org.apache.tomcat.util.http.LegacyCookieProcessor;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 
+ * 
+ * @author Gregoire Jeanmart <gregoire.jeanmart@consensys.net>
+ *
+ */
+@Configuration
+public class WebConfiguration {
+    
+    @Bean
+    public EmbeddedServletContainerCustomizer customizer() {
+        return container -> {
+            if (container instanceof TomcatEmbeddedServletContainerFactory) {
+                TomcatEmbeddedServletContainerFactory tomcat = (TomcatEmbeddedServletContainerFactory) container;
+                tomcat.addContextCustomizers(context -> context.setCookieProcessor(new LegacyCookieProcessor()));
+            }
+        };
+    }
+}
