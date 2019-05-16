ee5c9a2e759c0c75ccaf7e8b2828d192d785eaf5
gjeanmart <gregoire.jeanmart@gmail.com>
2019-05-16 14:28:22 +0100

    Organisation - Fixed equal & hashCode

--- common/src/main/java/net/consensys/web3auth/common/dto/Organisation.java
+++ common/src/main/java/net/consensys/web3auth/common/dto/Organisation.java
@@ -23,7 +23,7 @@ public class Organisation implements Serializable {
         this.role = role;
         this.dateAdded = new Date();
     }
-    
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
@@ -38,17 +38,16 @@ public class Organisation implements Serializable {
                 return false;
         } else if (!name.equals(other.name))
             return false;
-        if (role != other.role)
-            return false;
         return true;
     }
+
+
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((name == null) ? 0 : name.hashCode());
-        result = prime * result + role;
         return result;
     }
-
+    
 }
